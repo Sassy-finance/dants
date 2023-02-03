21d8c1d026148fdbaaca61c693bc2e7effd5d497
Ialberquilla <ivan.alberquilla@gmail.com>
2023-02-03 10:29:51 +0000

    mint nft

--- contracts/contracts/PipelineShop.sol
+++ contracts/contracts/PipelineShop.sol
@@ -3,20 +3,24 @@ pragma solidity ^0.8.9;
 
 // import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./DantsNFT.sol";
 
 contract PipelineShop {
     mapping(uint256 => mapping(address => bool)) public pipelines;
     address public tokenAddress;
+    address public nftAddress;
 
     event PipelinePayed(uint256 pipelineId, address user);
 
-    constructor(address _tokenAddress) {
+    constructor(address _tokenAddress, address _nftAddress) {
         tokenAddress = _tokenAddress;
+        nftAddress = _nftAddress;
     }
 
     function buyPipeline(uint256 pipelineId, uint256 price) external {
         IERC20(tokenAddress).transferFrom(msg.sender, address(this), price);
         pipelines[pipelineId][msg.sender] = true;
+        DANTs(nftAddress).safeMint(msg.sender);
         emit PipelinePayed(pipelineId, msg.sender);
     }
 
